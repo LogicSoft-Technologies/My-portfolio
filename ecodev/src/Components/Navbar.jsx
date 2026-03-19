5306fa030920b77c38752970b74bb968ee3fc01c
Elijah Alexander <elijahkylealexander@gmail.com>
2026-03-19 10:46:25 +0100

    hello

--- ecodev/src/Components/Navbar.jsx
+++ ecodev/src/Components/Navbar.jsx
@@ -386,11 +386,11 @@ const Navbar = () => {
   }, [])
 
   useEffect(() => {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches))
-      document.documentElement.classList.add('dark')
-    else
-      document.documentElement.classList.remove('dark')
-  }, [])
+  if (localStorage.theme === 'dark')
+    document.documentElement.classList.add('dark')
+  else
+    document.documentElement.classList.remove('dark')
+}, [])
 
   function toggleTheme() {
     document.documentElement.classList.toggle('dark')
